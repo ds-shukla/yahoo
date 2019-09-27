e87841a83ff9e4ffcbd7972fcd48f0d07da33acb
batch299 <krishnamohanvenkat9@gmail.com>
2019-09-27 08:11:08 +0530

    updated again test

--- myproj/src/Yahoo_Prg/Test.java
+++ myproj/src/Yahoo_Prg/Test.java
@@ -6,6 +6,7 @@ public class Test
 	public static void main(String[] args) 
 	{
        System.out.println("sample test code");
+       System.out.println("This is line is updateed by venkat");
 
 	}
 
